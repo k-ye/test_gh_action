6c993733a677a3d270307a3841386baed91d3820
Ye Kuang <k-ye@users.noreply.github.com>
2021-01-11 12:26:07 +0900

    [long] trigger on release creation (#8)
    * [long] trigger on release creation
    
    * [long] tweak

--- run_long.py
+++ run_long.py
@@ -1,6 +1,13 @@
 import time
 import logging
+import sys
 
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
+                    level=logging.DEBUG,
+                    datefmt='%Y-%m-%d %H:%M:%S')
+argv = sys.argv
+if len(argv) > 1:
+  logging.info(argv)
 for i in range(20):
   logging.info('running %d', i)
   time.sleep(5)
